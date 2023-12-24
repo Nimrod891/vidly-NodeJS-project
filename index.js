7874255c66689739c40368afa22e941c5b14d774
Nimrod891 <62762890+Nimrod891@users.noreply.github.com>
2023-12-24 17:11:43 +0200

    finish get one, get all, update, delete endpoints for genres

--- index.js
+++ index.js
@@ -9,18 +9,47 @@ const genres = [
   { id: 2, name: "Action" },
   { id: 3, name: "Drama" },
 ];
+//Getting all genres
+app.get("/api/genres", (req, res) => {
+  res.send(genres);
+});
 
 //Creating a genre
+app.post("/api/genres", (req, res) => {
+  const { error } = validateGenre(req.body);
 
+  if (error) return res.status(400).send(error.details[0].message);
 
-//Getting all genres
+  const genre = {
+    id: genres.length + 1,
+    name: req.body.name,
+  };
+  genres.push(genre);
+  res.send(genre);
+});
 
 //Getting a single genre
+app.get("/api/genre/:id", (req, res) => {
+  const genre = genres.find((c) => c.id === parseInt(req.params.id));
 
+  if (!genre) return res.status(404).send("Genre ID does not exist");
+  res.send(genre);
+});
 //Updating a genre
+const genre = genres.find((c) => c.id === parseInt(req.body.id));
+if (!genre) return res.status(404).send("Genre ID does not exist");
 
 //Deleting a genre
+app.delete("api/genres/:id", (req, res) => {
+  const genre = genres.find((c) => c.id === parseInt(req.body.id));
+  if (!genre) return res.status(404).send("Genre ID does not exist");
+
+  const index = genres.indexOf(genre);
+  genres.splice(index, 1);
+
+  res.send(genre);
+});
 
 // Env variable for port, default = 3000 if port is undefied
-const port=process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
